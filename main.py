975e6fb57615f621d6c1e4ba7cdccb215ea31501
kelvin <kelvin.morcillo@gmail.com>
2025-05-06 11:25:44 -0300

    login update1

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def  index():
     <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
-    <title>replit</title>
+    <title>login teste 1</title>
     <link href="/staticstyle.css" rel="stylesheet" type="text/css" />
     </head>
 
